bf839eb080b14ae075875bf274afc883259b344e
ryanlester91 <lestertr@dukes.jmu.edu>
2024-11-16 19:35:58 -0500

    created 3 rows of blue blocks

--- index.js
+++ index.js
@@ -14,7 +14,22 @@ class Block {
 
 //all my blocks, array
 const blocks = [
-    new Block(10, 270)
+    new Block(10, 270),
+    new Block(120, 270),
+    new Block(230, 270),
+    new Block(340, 270),
+    new Block(450, 270),
+    new Block(10, 240),
+    new Block(120, 240),
+    new Block(230, 240),
+    new Block(340, 240),
+    new Block(450, 240),
+    new Block(10, 210),
+    new Block(120, 210),
+    new Block(230, 210),
+    new Block(340, 210),
+    new Block(450, 210),
+    
 ]
 
 //draw all my blocks
@@ -22,8 +37,8 @@ function addBlocks() {
     for (let i = 0; i < blocks.length; i++){
         const block = document.createElement('div')
         block.classList.add('block')
-        grid.style.left = blocks[i]
-        grid.style.bottom = '50px'
+        block.style.left = blocks[i].bottomLeft[0] + 'px'
+        block.style.bottom = blocks[i].bottomLeft[1] + 'px'
         grid.appendChild(block)
 
     }
